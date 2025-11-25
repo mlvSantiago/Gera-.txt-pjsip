a29887d9c3744dbcf77e7a2c017208a99689c637
Maria Luisa Santiago Vieira <marialuisasantiagoccp@gmail.com>
2025-11-25 09:36:50 -0300

    Corrigindo Gerar Txt

--- src/geratxt.py
+++ src/geratxt.py
@@ -4,7 +4,16 @@ import secrets # Para gerar Senhas aleatorias
 import os
 
 
-# Abre arquivo para a escrita
+
+def leia_int(msg):
+    while True:
+        try:
+            return int(input(msg))
+        except ValueError:
+            print("ERRO: Digite um número válido!")
+
+
+            
 
 def geraTxt():
 
@@ -15,92 +24,89 @@ def geraTxt():
     
 
 
-    try:
     
-    # call_group = int(input("CallGroup: "))
-        pick_group = int(input("Servidor: "))
-        contexto = int(input("Contexto do Condominio (Ex: 22): "))
-
-        ap_andar = int(input("Numero de andar por prédio: "))
+    pick_group = leia_int("Servidor: ")
+    contexto = leia_int("Contexto do Condominio (Ex: 22): ")
+    ap_andar = leia_int("Numero de andar por prédio: ")
 
 
-        print("Numeração das unidades nos andares (ex.: faixas: 10-18, 101-118, etc.)")
-
-        faixas = []
-
-        for i in range(ap_andar):
-            entrada = input(f"Faixa {i+1}: ")          
-
-            while '-' not in entrada:
-                print("\nSEPARADOR '-' AUSENTE!\n")
-                entrada = input(f"Faixa {i+1}: ")     
-
-        
-            partes = entrada.split("-")               # divide: ["10 ", " 18"]
-            
-            # strip em cada parte
-            inicio = int(partes[0].strip())
-            fim = int(partes[1].strip())
-
-            faixas.append((inicio, fim))
-
-        # Criando ramais
-        ramal = []
-        display = []
-
-        ramaisChamadores = []
-        displayChamadores = []
-
-        for inicio , fim in faixas:
+    print("Numeração das unidades nos andares (ex.: faixas: 10-18, 101-118, etc.)")
+    faixas = []
 
 
+    for i in range(ap_andar):
+        entrada = input(f"Faixa {i+1}: ")          
 
-            for i in range(inicio, fim + 1):
+        while '-' not in entrada:
+            print("\nSEPARADOR '-' AUSENTE!\n")
+            entrada = input(f"Faixa {i+1}: ")     
 
-            
-
-                    for j in range(1,5):
-
-                        match j:
-                            case 1:
-                                letra = 'A'
-                            case 2:
-                                letra = 'B'
-                            case 3:
-                                letra = 'C'
-                            case 4 :
-                                letra = 'D'
-
-                        ramal.append(str(contexto) + str(i) + "0" + str(j))
-                        display.append("Apto " +  str(i) +" " + letra)
-                    ramaisChamadores.append(str(contexto) + str(i))
-                    displayChamadores.append("Chamador " + str(i) + " ")
-                    print(i)
-
-
-        # Abre arquivo
-        output_dir = f"../output/Condominio-{contexto}"
-        os.makedirs(output_dir, exist_ok=True)
-        saida = open(f"{output_dir}/pjsip_additional.txt" , "w")
-        
-        qt = len(ramal)
-        #Tratando de senhas
-        allPassword = []
     
-        caracteres = string.ascii_letters + string.digits
-        for i in range(qt):
-            while True:
-                password = ''.join(secrets.choice(caracteres) for i in range(9))
-                if (any(c.islower() for c in password)
-                        and any(c.isupper() for c in password)
-                        and sum(c.isdigit() for c in password) >= 3):
-                    break
-            allPassword.append(password)
-        print("\n\n\n----------GERANDO PJSIP_ADDITIONAL.TXT-----------\n\n\n")
-
-        for i in range(qt):
+        partes = entrada.split("-")               # divide: ["10 ", " 18"]
         
-            padrao = f'''[{ramal[i]}]
+        # strip em cada parte
+        inicio = int(partes[0].strip())
+        fim = int(partes[1].strip())
+
+        faixas.append((inicio, fim))
+
+    # Criando ramais
+    ramal = []
+    display = []
+
+    ramaisChamadores = []
+    displayChamadores = []
+
+    for inicio , fim in faixas:
+
+
+
+        for i in range(inicio, fim + 1):
+
+        
+
+                for j in range(1,5):
+
+                    match j:
+                        case 1:
+                            letra = 'A'
+                        case 2:
+                            letra = 'B'
+                        case 3:
+                            letra = 'C'
+                        case 4 :
+                            letra = 'D'
+
+                    ramal.append(str(contexto) + str(i) + "0" + str(j))
+                    display.append("Apto " +  str(i) +" " + letra)
+                ramaisChamadores.append(str(contexto) + str(i))
+                displayChamadores.append("Chamador " + str(i) + " ")
+                print(i)
+
+
+    # Abre arquivo
+    output_dir = f"../output/Condominio-{contexto}"
+    os.makedirs(output_dir, exist_ok=True)
+    saida = open(f"{output_dir}/pjsip_additional.txt" , "w")
+    
+    qt = len(ramal)
+    #Tratando de senhas
+    allPassword = []
+
+    caracteres = string.ascii_letters + string.digits
+    for i in range(qt):
+        while True:
+            password = ''.join(secrets.choice(caracteres) for i in range(9))
+            if (any(c.islower() for c in password)
+                    and any(c.isupper() for c in password)
+                    and sum(c.isdigit() for c in password) >= 3):
+                break
+        allPassword.append(password)
+    print("\n\n\n----------GERANDO PJSIP_ADDITIONAL.TXT-----------\n\n\n")
+
+    for i in range(qt):
+    
+        padrao = f'''[{ramal[i]}]
 auth=auth{ramal[i]}
 aors={ramal[i]}
 type=endpoint
@@ -145,21 +151,21 @@ password={allPassword[i]}
 [{ramal[i]}]
 type=aor
 qualify_frequency=60
-ax_contacts=1
+max_contacts=1
 remove_existing=no
 qualify_timeout=3.0
 authenticate_qualify=no
 '''
-            saida.write(padrao + "\n")
+        saida.write(padrao + "\n")
 
-        saida.write("; =============================\n; Ramais Chamadores\n; =============================")
+    saida.write("; =============================\n; Ramais Chamadores\n; =============================")
 
-        
-        for i in range(len(ramaisChamadores)):
-            padraoChamador = f'''\n
+    
+    for i in range(len(ramaisChamadores)):
+        padraoChamador = f'''\n
 [{ramaisChamadores[i]}]
 auth=auth{ramaisChamadores[i]}
-aors={ramaisChamadores[i]}s
+aors={ramaisChamadores[i]}
 type=endpoint
 language=pt_BR
 deny=0.0.0.0/0.0.0.0
@@ -206,19 +212,8 @@ max_contacts=1
 remove_existing=yes
 qualify_timeout=3.0
 authenticate_qualify=no'''
-            saida.write(padraoChamador + "\n")
-        
-    except FileExistsError:
-        print("Erro: Problema ao abrir arquivo para escrita")
+        saida.write(padraoChamador + "\n")
+    
 
-    except ValueError:
-        print("ERRO: Os dados fornecidos devem ser numericos")
-        raise
-    except FileNotFoundError:
-        print("ERRO: Arquivo não encontrado")
-                    
-    finally:
-        if saida is not None:
-            saida.close()
 
     return ramal, display, allPassword, contexto , pick_group
